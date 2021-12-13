045dcfd9bf0eb462d2c45df92c129658680db4c4
samismail <samismail2010@gmail.com>
2021-12-13 06:54:19 -0600

    first commit

---
+++ develop/script.js
@@ -0,0 +1,31 @@
+// Assignment code here
+
+var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var numbers = ["0","1","2","3","4","5","6","7","8","9"]
+var special = ["!","@","#","$","%","^","&","*","-","_","=","+"]
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+function generatePassword(){
+  var length = prompt("Please generate a password containing between 8 and 128 characters");
+  if(isNaN(length)) {
+      alert("Password must include a number")
+      return generatePassword()
+  }
+  
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword) 
