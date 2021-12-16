8c95e6380fbdb7c26e9712f0d4782839758b8e4a
samismail <samismail2010@gmail.com>
2021-12-15 19:33:46 -0600

    added additinal prompt

--- develop/script.js
+++ develop/script.js
@@ -1,13 +1,11 @@
 // Assignment code here
 
-var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var numbers = ["0","1","2","3","4","5","6","7","8","9"]
-var special = ["!","@","#","$","%","^","&","*","-","_","=","+"]
-var available =[];
+
+
+
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate")
 
 // Write password to the #password input
 function writePassword() {
@@ -19,9 +17,19 @@ function writePassword() {
 }
 
 function generatePassword(){
+
+    var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    var numbers = ["0","1","2","3","4","5","6","7","8","9"]
+    var special = ["!","@","#","$","%","^","&","*","-","_","=","+"]
+    var available = [...uppercase, ...lowercase, ...numbers, ...special];
+    var arr = []
+    console.log(available.length)
+
+
   var length = prompt("Please generate a password containing between 8 and 128 characters");
-  if(isNaN(length)) {
-      alert("Password must include a number")
+  if(length < 8 || length > 128) {
+      window.alert("Password must include a number")
       return generatePassword()
   }
   //type of characters needednfor generator
@@ -30,23 +38,15 @@ function generatePassword(){
   var hasNumbers = confirm("Does this password include numbers?");
   var hasSpecial = confirm("Does this password include special characters?");
 
-  if (hasUppercase === true) {
-    available = available.length(uppercase);
+  for(var i = 0; i < available.length; i++) {
+    var picker = Math.floor(Math.random()* available.length);
+    arr.push(available.length[picker])
+    
   }
-  if (hasLowercase === true){
-    available = available.length(lowercase);
-  }
-  if(hasNumbers === true) {
-    available = available.length(numbers);
-  }
-  if(hasSpecial === true) {
-    available = available.length(special);
-  }
-  for(var i = 0; i < length;) {
-    password = password + available[Math.floor(Math.random()*available.length)];
-    i++;
-  }
-}
+  arr.length = 10
+  console.log(arr.join(""))
 
+console.log(generatePassword())
+}
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword) 
+generateBtn.addEventListener("click", generatePassword); 
